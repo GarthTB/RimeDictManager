aa3580387f8e8739834123699d10c37c94804f76
Garth TB | 天卜 <g-art-h@outlook.com>
2026-03-23 11:08:09 +0800

    实现主窗口UI布局和交互

--- RimeDictManager/Views/MainWindow.xaml.cs
+++ RimeDictManager/Views/MainWindow.xaml.cs
@@ -1,5 +1,25 @@
 ﻿namespace RimeDictManager.Views;
 
+using System.Windows;
+using VMs;
+
 public sealed partial class MainWindow {
-    public MainWindow() => InitializeComponent();
+    public MainWindow() {
+        InitializeComponent();
+        Closing += (_, e) => { e.Cancel = DataContext is MainVM { UnmodOrDiscard: false }; };
+    }
+
+    private void DropDict(object _, DragEventArgs e) {
+        if (DataContext is MainVM { UnmodOrDiscard: true } vm
+         && e.Data.GetData(DataFormats.FileDrop) is string[] and [var path])
+            vm.LoadDict(path);
+    }
+
+    private void DropCharsDict(object _, DragEventArgs e) {
+        if (DataContext is MainVM vm
+         && e.Data.GetData(DataFormats.FileDrop) is string[] and [var path])
+            vm.SetEncoder(path);
+    }
+
+    private void ShowLogWindow(object _, RoutedEventArgs e) => new LogWindow().ShowDialog();
 }
