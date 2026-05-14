42da5bfa99e0769cb8cb10ace8066fe6886e523b
Garth TB | 天卜 <g-art-h@outlook.com>
2026-05-14 11:10:12 +0800

    重构日志模块

--- RimeDictManager/Services/Logger.cs
+++ RimeDictManager/Services/Logger.cs
@@ -2,24 +2,22 @@ namespace RimeDictManager.Services;
 
 using System.IO;
 using Models;
-using static DateTime;
 
 internal static class Logger {
-    private static readonly List<string> Logs = new(64) { $"{Now:u} 日志开始" };
-    public static IReadOnlyList<string> LogView => Logs;
+    private static readonly List<string> Logs = new(1024) { $"{DateTime.Now:u} 日志开始" };
+    public static uint Count { get; private set; }
+    public static string Dump() => string.Join('\n', Logs);
 
-    /// <summary> 添加日志 </summary>
-    /// <param name="msg"> 信息 </param>
-    /// <param name="line"> 涉及的词库行 </param>
-    public static void Log(string msg, Line? line) =>
-        Logs.Add(
-            line is {}
-                ? $"{msg}\t{line}"
-                : msg);
+    public static void Log(string msg, Entry? e) {
+        if (e is {} v) msg += $"\t{LineCodec.Serialize(v)}";
+        Logs.Add(msg);
+        Count++;
+    }
 
     public static void Save(string path) {
-        using StreamWriter sw = new(path, true);
-        sw.NewLine = "\n";
-        foreach (var line in Logs.Append($"{Now:u} 日志保存")) sw.WriteLine(line);
+        using StreamWriter writer = new(path, true);
+        writer.NewLine = "\n";
+        Logs.Add($"{DateTime.Now:u} 日志导出");
+        foreach (var l in Logs) writer.WriteLine(l);
     }
 }
--- RimeDictManager/VMs/LogVM.cs
+++ RimeDictManager/VMs/LogVM.cs
@@ -1,37 +1,33 @@
 namespace RimeDictManager.VMs;
 
 using System.IO;
-using System.Windows;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Microsoft.Win32;
 using Services;
-using static System.Windows.MessageBox;
-using static System.Windows.MessageBoxButton;
-using static System.Windows.MessageBoxImage;
-using static Services.Logger;
+using static System.Windows.MessageBoxResult;
+using static MsgBox;
 
 internal sealed partial class LogVM: ObservableObject {
-    public static string LogText => string.Join('\n', LogView);
-    private static bool HasLogs => LogView.Count > 1;
+    public static string Logs => Logger.Dump();
+    private static bool Any => Logger.Count > 0;
 
-    [RelayCommand(CanExecute = nameof(HasLogs))]
+    [RelayCommand(CanExecute = nameof(Any))]
     private static void Save() {
         try {
             var sfd = new SaveFileDialog {
-                Title = "将日志保存为...",
-                FileName = $"RDM_{DateTime.Now:yyMMdd-HHmmss}.log",
+                Title = "将日志保存至...",
+                FileName = $"RDM_{DateTime.Now:yyMMdd_HHmmss}.log",
                 Filter = "日志文件|*.log|所有文件|*.*",
                 OverwritePrompt = false
             };
             if (sfd.ShowDialog() != true) return;
             var path = sfd.FileName;
-            if (File.Exists(path)) {
-                var msg = $"确认追加此文件？\n'{path}'";
-                if (Show(msg, "确认", YesNo, Question) != MessageBoxResult.Yes) return;
-            }
+            var msg = $"确认追加此文件？\n'{path}'";
+            if (File.Exists(path) && ShowConfirm("确认", msg) != Yes) return;
+
             Logger.Save(path);
-            Show($"已将{LogView.Count - 1}条日志写入'{path}'", "成功", OK, Information);
-        } catch (Exception ex) { Show($"保存日志时：\n{ex}", "异常", OK, Error); }
+            ShowInfo("成功", $"日志已写入'{path}'，共{Logger.Count}条");
+        } catch (Exception ex) { ShowErr($"保存日志时：\n{ex}"); }
     }
 }
