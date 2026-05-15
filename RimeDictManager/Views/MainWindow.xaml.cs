01e0206e2c7f5180de4c441fd97aee6766e0e9c3
Garth TB | 天卜 <g-art-h@outlook.com>
2026-05-15 09:00:08 +0800

    重构主窗口界面

--- RimeDictManager/Views/MainWindow.xaml.cs
+++ RimeDictManager/Views/MainWindow.xaml.cs
@@ -6,20 +6,19 @@ using VMs;
 public sealed partial class MainWindow {
     public MainWindow() {
         InitializeComponent();
-        Closing += (_, e) => { e.Cancel = DataContext is MainVM { UnmodOrDiscard: false }; };
+        Closing += (_, e) => e.Cancel = !((MainVM)DataContext).EnsureSaved;
     }
 
     private void DropDict(object _, DragEventArgs e) {
-        if (DataContext is MainVM { UnmodOrDiscard: true } vm
-         && e.Data.GetData(DataFormats.FileDrop) is string[] and [var path])
+        var vm = (MainVM)DataContext;
+        if (vm.EnsureSaved && e.Data.GetData(DataFormats.FileDrop) is string[] and [var path])
             vm.LoadDict(path);
     }
 
     private void DropCharsDict(object _, DragEventArgs e) {
-        if (DataContext is MainVM vm
-         && e.Data.GetData(DataFormats.FileDrop) is string[] and [var path])
-            vm.SetEncoder(path);
+        if (e.Data.GetData(DataFormats.FileDrop) is string[] and [var path])
+            ((MainVM)DataContext).SetEncoder(path);
     }
 
-    private void ShowLogWindow(object _, RoutedEventArgs e) => new LogWindow().ShowDialog();
+    private void ShowLogs(object _, RoutedEventArgs e) => new LogWindow().ShowDialog();
 }
