71878c733c757c864d9a1ef41a849ff3bc64cf05
Garth TB | 天卜 <g-art-h@outlook.com>
2026-05-16 19:28:06 +0800

    优化LogVM

--- RimeDictManager/VMs/LogVM.cs
+++ RimeDictManager/VMs/LogVM.cs
@@ -14,18 +14,16 @@ internal sealed partial class LogVM: ObservableObject {
 
     [RelayCommand(CanExecute = nameof(Any))]
     private static void Save() {
+        var sfd = new SaveFileDialog {
+            Title = "将日志保存至...",
+            FileName = $"RDM_{DateTime.Now:yyMMdd_HHmmss}.log",
+            Filter = "日志文件|*.log|所有文件|*.*",
+            OverwritePrompt = false
+        };
+        if (sfd.ShowDialog() != true) return;
+        var path = sfd.FileName;
+        if (File.Exists(path) && ShowConfirm("确认", $"追加此文件？\n'{path}'") != Yes) return;
         try {
-            var sfd = new SaveFileDialog {
-                Title = "将日志保存至...",
-                FileName = $"RDM_{DateTime.Now:yyMMdd_HHmmss}.log",
-                Filter = "日志文件|*.log|所有文件|*.*",
-                OverwritePrompt = false
-            };
-            if (sfd.ShowDialog() != true) return;
-            var path = sfd.FileName;
-            var msg = $"确认追加此文件？\n'{path}'";
-            if (File.Exists(path) && ShowConfirm("确认", msg) != Yes) return;
-
             Logger.Save(path);
             ShowInfo("成功", $"日志已写入'{path}'，共{Logger.Count}条");
         } catch (Exception ex) { ShowErr($"保存日志时：\n{ex}"); }
