66e8634f7d769108653a368ef5035ab69275c4a1
Garth TB | 天卜 <g-art-h@outlook.com>
2026-06-12 10:14:05 +0800

    优化词库管理服务

--- RimeDictManager/Services/DictManager.cs
+++ RimeDictManager/Services/DictManager.cs
@@ -1,18 +1,30 @@
 namespace RimeDictManager.Services;
 
 using Models;
+using ViewModels;
 
 public static class DictManager {
     /// <summary> 词库集合：首项为加词目标 </summary>
     private static readonly List<Dict> Dicts = [];
 
-    public static IReadOnlyList<Dict> DictList => Dicts;
-    public static void AddDict(Dict dict) => Dicts.Add(dict);
-    public static bool RemoveDict(Dict dict) => Dicts.Remove(dict);
+    public static IEnumerable<DictInfo> DictInfos => Dicts.Select(static x => new DictInfo(x));
 
-    public static Dict SetTgtDict(Dict dict) {
-        var i = Dicts.IndexOf(dict);
+    public static DictInfo AddDict(string path) {
+        if (Dicts.Any(x => x.Path == path)) throw new InvalidOperationException("单字码表重复");
+        Dict dict = new(path);
+        Dicts.Add(dict);
+        return new(dict);
+    }
+
+    public static void RemoveDict(DictInfo dict) {
+        if (Dicts.RemoveAll(x => x.Path == dict.Path) == 0)
+            throw new InvalidOperationException("找不到要移除的词库");
+    }
+
+    public static string SetTgtDict(DictInfo dict) {
+        if (Dicts.FindIndex(x => x.Path == dict.Path) is not (>= 0 and var i))
+            throw new InvalidOperationException("找不到要设为加词目标的词库");
         (Dicts[0], Dicts[i]) = (Dicts[i], Dicts[0]);
-        return Dicts[i];
+        return Dicts[i].Path;
     }
 }
--- RimeDictManager/ViewModels/DictInfo.cs
+++ RimeDictManager/ViewModels/DictInfo.cs
@@ -3,11 +3,13 @@ namespace RimeDictManager.ViewModels;
 using CommunityToolkit.Mvvm.ComponentModel;
 using Models;
 
-/// <summary> 词库信息：DataGrid专供 </summary>
+/// <summary> 词库信息：隔离词库和UI </summary>
 public sealed partial class DictInfo(Dict src): ObservableObject {
-    public Dict Src { get; } = src;
     public string Name { get; } = src.Name;
+
+    /// <summary> 路径：唯一标识 </summary>
     public string Path { get; } = src.Path;
+
     public uint Cnt { get; } = src.Cnt;
 
     [ObservableProperty, NotifyPropertyChangedFor(nameof(ModText))]
@@ -15,16 +17,16 @@ public sealed partial class DictInfo(Dict src): ObservableObject {
 
     public string ModText =>
         Mod
-            ? "有"
-            : "无";
+            ? "●"
+            : "○";
 
     [ObservableProperty, NotifyPropertyChangedFor(nameof(TgtText))]
     public partial bool Tgt { get; private set; }
 
     public string TgtText =>
         Tgt
-            ? "是"
-            : "否";
+            ? "●"
+            : "";
 
     public void NotifySaved() => Mod = false;
     public void SetTgt(bool tgt) => Tgt = tgt;
