0f4d64057ede52d362276b866c0d3175cbe33463
Garth TB | 天卜 <g-art-h@outlook.com>
2026-06-15 14:40:09 +0800

    有修改时阻止退出

--- RimeDictManager/Views/MainWindow.axaml.cs
+++ RimeDictManager/Views/MainWindow.axaml.cs
@@ -4,9 +4,11 @@ using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Services;
 using ViewModels;
+using ZLinq;
 
 public sealed partial class MainWindow: Window {
     private readonly MainWindowVM _vm = new();
+    private bool _closeConfirmed;
 
     public MainWindow() {
         InitializeComponent();
@@ -23,5 +25,19 @@ public sealed partial class MainWindow: Window {
         try { await new LogWindow().ShowDialog(this); } catch (Exception ex) {
             await ex.Alert("查看日志", this);
         }
+    }
+
+    protected override async void OnClosing(WindowClosingEventArgs e) {
+        try {
+            if (!_closeConfirmed
+             && DictManager.AllDicts.AsValueEnumerable().Any(static x => x.Modified)) {
+                e.Cancel = true;
+                if (await MsgBox.Ask<bool>("有词库的变更未保存，是否丢弃？")) {
+                    _closeConfirmed = true;
+                    Close();
+                }
+            }
+            base.OnClosing(e);
+        } catch (Exception ex) { await ex.Alert("阻止退出", this); }
     }
 }
