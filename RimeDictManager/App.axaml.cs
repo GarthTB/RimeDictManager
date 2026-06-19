4ff75c4e1025ce1ff12e91e9f8f18f690fd82f5b
Garth TB | 天卜 <g-art-h@outlook.com>
2026-06-20 03:24:14 +0800

    优化代码

--- RimeDictManager/App.axaml.cs
+++ RimeDictManager/App.axaml.cs
@@ -1,21 +1,23 @@
 namespace RimeDictManager;
 
 using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using Views;
-using Desktop = Avalonia.Controls.ApplicationLifetimes.IClassicDesktopStyleApplicationLifetime;
 
 public sealed class App: Application {
     public override void Initialize() => AvaloniaXamlLoader.Load(this);
 
     public override void OnFrameworkInitializationCompleted() {
-        (ApplicationLifetime as Desktop)?.MainWindow = new MainWindow();
-#if MACOS
+        (ApplicationLifetime as IClassicDesktopStyleApplicationLifetime)?.MainWindow
+            = new MainWindow();
+
+        // macOS专用
         this.TryGetFeature<IActivatableLifetime>()?.Activated += static (_, e) => {
             if (e is ProtocolActivatedEventArgs { Kind: ActivationKind.OpenUri } args)
                 UrlActivation.ParseUrl(args.Uri.ToString());
         };
-#endif
+
         base.OnFrameworkInitializationCompleted();
     }
 }
