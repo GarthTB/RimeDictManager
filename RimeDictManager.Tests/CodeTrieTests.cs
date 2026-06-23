079e689cf7f53d043f27a786f67e021e04dd3c91
Garth TB | 天卜 <g-art-h@outlook.com>
2026-06-23 19:40:11 +0800

    添加测试项目

---
+++ RimeDictManager.Tests/CodeTrieTests.cs
@@ -0,0 +1,25 @@
+﻿namespace RimeDictManager.Tests;
+
+using Models;
+using static Assert;
+
+public sealed class CodeTrieTests {
+    [Fact]
+    public void Indexer() {
+        CodeTrie trie = new(16);
+
+        var v0 = trie["abc"];
+
+        Null(v0);
+
+        trie.Insert("abc", 1);
+
+        var v1 = trie["abc"];
+
+        NotNull(v1);
+        Single(v1);
+        Equal(1, v1[0]);
+        Null(trie["abd"]);
+        Null(trie["ab"]);
+    }
+}
